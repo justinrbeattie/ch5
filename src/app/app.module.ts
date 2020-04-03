eba5f487a0b03de132facb416c0a11876ce102df
Justin Beattie <justinbeattie@Justins-MacBook-Pro.local>
2020-04-03 17:11:17 +1100

    grid component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridTestComponent } from './test-components/grid-test/grid-test.component';
+import { GridComponent } from './shared/grid/grid.component';
+import { CellComponent } from './shared/cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridTestComponent,
+    GridComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule,
